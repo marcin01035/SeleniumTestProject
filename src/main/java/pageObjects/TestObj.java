5900c4dcc7e8f15f84bb3fb307538c4040e401f0
Marcin Miśkowiec <miskowiec@veloxalpha.com>
2023-04-26 22:35:06 +0200

    add allure report

---
+++ src/main/java/pageObjects/TestObj.java
@@ -0,0 +1,16 @@
+package pageObjects;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TestObj {
+    private String test1;
+    private String test2;
+
+}
--- src/test/java/exampleTest/TestHelper.java
+++ src/test/java/exampleTest/TestHelper.java
@@ -1,4 +1,6 @@
+package exampleTest;
 
+import io.qameta.allure.Step;
 import utils.ObjectsRepo;
 import utils.PropertiesReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -10,18 +12,21 @@ public class TestHelper extends ObjectsRepo{
 
 
     @BeforeSuite
+    @Step("Set environment")
     public void setupEnvironment() {
         WebDriverManager.chromedriver().setup();
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10));
     }
 
     @BeforeTest
+    @Step("Open browser and go to website")
     public void openBrowser() {
         driver.manage().window().maximize();
         driver.get(PropertiesReader.getProperty("test.baseUrl"));
     }
 
     @AfterSuite
+    @Step("Close website browser")
     public void closeEnvironment() {
         driver.quit();
     }
